28aa02920cef023374576066f42a688d2a0eaa76
akiranszw <akira@nszw.jp>
2013-07-01 15:24:14 +0900

    change city

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'katsushika_budget2013';
+/* OpenSpending.identifier = 'katsushika_budget2013'; */
+OpenSpending.identifier = 'yokohama_budget2012';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
