64617a081aef152bb9ad404d925809a44224370e
akiranszw <akira@nszw.jp>
2013-07-01 17:02:25 +0900

    choice another identifier

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'katsushika_budget2013';
+OpenSpending.identifier = 'katsushika_ward_budget2013';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
